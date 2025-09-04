f92ba76469204ebc292961801af5881fcd7a954e
jgallenrenau <jordigr.bcn@bcngmail.com>
2025-09-04 11:50:34 +0200

    chore(data): normalize Data Package.swift formatting to fix trailing whitespace in CI

--- Viaplay/Viaplay/Base/Data/Package.swift
+++ Viaplay/Viaplay/Base/Data/Package.swift
@@ -3,15 +3,26 @@ import PackageDescription
 
 let package = Package(
     name: "Data",
-    platforms: [ .iOS(.v16), .macOS(.v12) ],
-    products: [ .library(name: "Data", targets: ["Data"]) ],
+    platforms: [
+        .iOS(.v16),
+        .macOS(.v12)
+    ],
+    products: [
+        .library(name: "Data", targets: ["Data"])
+    ],
     dependencies: [
         .package(path: "../Domain"),
         .package(path: "../NetworkingKit"),
         .package(path: "../StorageKit")
     ],
     targets: [
-        .target(name: "Data", dependencies: ["Domain", "NetworkingKit", "StorageKit"]),
-        .testTarget(name: "DataTests", dependencies: ["Data"])
+        .target(
+            name: "Data",
+            dependencies: ["Domain", "NetworkingKit", "StorageKit"]
+        ),
+        .testTarget(
+            name: "DataTests",
+            dependencies: ["Data"]
+        )
     ]
 )
